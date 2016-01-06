723ab0bbc71483d4a21edddf3872b47e6ea8fbef
Allan Vialatte <allan.vialatte@icloud.com>
2016-01-06 11:23:46 +0100

    * add good header * remove bridging

--- Shimmer/Shimmer.h
+++ Shimmer/Shimmer.h
@@ -16,4 +16,6 @@ FOUNDATION_EXPORT const unsigned char ShimmerVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Shimmer/PublicHeader.h>
 
-
+#import <Shimmer/FBShimmering.h>
+#import <Shimmer/FBShimmeringView.h>
+#import <Shimmer/FBShimmeringLayer.h>
