5b6f1683155fe34d69382184c0e12b08123c2fe7
Jamie White <jamie@skim.it>
2016-04-28 11:28:29 +0100

    Creating shimmering framework

---
+++ Shimmering Framework/Shimmering.h
@@ -0,0 +1,22 @@
+//
+//  Shimmering Framework.h
+//  Shimmering Framework
+//
+//  Created by James White on 28/04/2016.
+//  Copyright © 2016 Facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Shimmering Framework.
+FOUNDATION_EXPORT double Shimmering_FrameworkVersionNumber;
+
+//! Project version string for Shimmering Framework.
+FOUNDATION_EXPORT const unsigned char Shimmering_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Shimmering_Framework/PublicHeader.h>
+
+
+#import <Shimmering/FBShimmering.h>
+#import <Shimmering/FBShimmeringView.h>
+#import <Shimmering/FBShimmeringLayer.h>
